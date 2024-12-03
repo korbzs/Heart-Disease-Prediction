7a095bbdf2984f67951e0ae38a790d25f406b2a2
korbzs <146817924+korbzs@users.noreply.github.com>
2024-12-03 10:54:13 +0100

    bugfix
    scaler problems

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,59 +60,75 @@ def run_webapp(model):
 
     #cp, trtrbps, exng, slp, caa, thall
     with st.form("heart_attack_form"):
-        # age = st.slider("Your Age [age]", 18, 100, value=30)
-        # sex = st.selectbox("Your Gender [sex]", [0, 1], format_func=lambda x: ["Women", "Men"][x], index=1)
-        cp = st.selectbox("Your Chest Pain Type [cp]", [0, 1, 2, 3],
-                          format_func=lambda x: ["asymptomatic (tünetmentes)", "atypical angina", "typical angina (H)", "non-anginal pain"][x], index=0)
-        trtbps = st.slider("Your Resting Blood Pressure [mm/Hg] [trtbps]", 90, 200, value=120, step=5)
-        # chol = st.slider("Your Cholesterol Measurement[mg/dl] [chol]", 200, 600, value= 400)
-        # st.text("Is Your Fasting Blood Sugar Greater Than 120 mg/dl? [fbs]")
-        # fbs = st.checkbox("Yes it is")
+        age = st.slider("Your Age [age]", 18, 100, value=30)
+        sex = st.selectbox("Your Gender [sex]", [0, 1], format_func=lambda x: ["Women", "Men"][x], index=1)
+        chol = st.slider("Your Cholesterol Measurement[mg/dl] [chol]", 200, 600, value= 400)
+        st.text("Is Your Fasting Blood Sugar Greater Than 120 mg/dl? [fbs]")
+        fbs = st.checkbox("Yes it is")
         
-        # restecg = st.selectbox("Your Resting ECG Results [restecg]", [0, 1, 2],
-        #                        format_func=lambda x: ["normal",
-        #                                             "having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)",
-        #                                             "showing probable or definite left ventricular hypertrophy by Estes’ criteria"][x],
-        #                        index=0
-        # )
-
-        # thalach = st.slider("Your Maximum Heart Rate [thalach]", 70, 200, value=145)
-        st.text("Have You Ever Experienced Angina When Exercising? [exng]")
-        exng = st.checkbox("Yes I have")
-        # oldpeak = st.slider("Your ST depression induced by exercise relative to rest [oldpeak]", 0.0, 6.0, value=3.0)
-        slp = st.selectbox("The slope of the peak exercise ST segment [slp]", [0, 1, 2], format_func=lambda x: ["downsloping (L)", "flat", "upsloping (H)"][x]
+        restecg = st.selectbox("Your Resting ECG Results [restecg]", [0, 1, 2],
+                               format_func=lambda x: ["normal",
+                                                    "having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)",
+                                                    "showing probable or definite left ventricular hypertrophy by Estes’ criteria"][x],
+                               index=0
         )
-        caa = st.slider("The number of major vessels (0–4) [caa]", 0, 4, value=1, step=1)
-        thall = st.selectbox("A blood disorder called thalassemia [thall]", [0, 1, 2],
+
+        oldpeak = st.slider("Your ST depression induced by exercise relative to rest [oldpeak]", 0.0, 6.0, value=3.0)
+        thalach = st.slider("Your Maximum Heart Rate [thalach]", 70, 200, value=145)
+
+        cp = st.selectbox("------ Your Chest Pain Type [cp] -----", [0, 1, 2, 3],
+                          format_func=lambda x: ["asymptomatic (tünetmentes)", "atypical angina", "typical angina (H)", "non-anginal pain"][x], index=0)
+        trtbps = st.slider("------ Your Resting Blood Pressure [mm/Hg] [trtbps] --------", 90, 200, value=120, step=5)
+        exng = st.checkbox("Yes I have")
+        st.text("------- Have You Ever Experienced Angina When Exercising? [exng] --------")
+        slp = st.selectbox("------- The slope of the peak exercise ST segment [slp] -------", [0, 1, 2], format_func=lambda x: ["downsloping (L)", "flat", "upsloping (H)"][x]
+        )
+        caa = st.slider("-------- The number of major vessels (0–4) [caa] --------", 0, 4, value=1, step=1)
+        thall = st.selectbox("-------- A blood disorder called thalassemia [thall] --------", [0, 1, 2],
                             format_func=lambda x: ["normal blood flow (L)",
                             "fixed defect (no blood flow in some part of the heart) (H)",
                             "reversible defect (a blood flow is observed but it is not normal)"][x]
         ) # add +1 at input data
-
+        #['cp', 'trtbps', 'exng', 'slp', 'caa', 'thall']
         submitted = st.form_submit_button("Submit")
 
         if submitted:
 
+#['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'oldpeak', 'slp', 'caa', 'thall']
             input_data = {
-                # "age": age, "sex": sex,
-                # "chol": chol, 
-                # "fbs": fbs, "restecg": restecg, "thalach": thalach,
-                # "oldpeak": oldpeak,
-                "cp": cp, "trtbps": trtbps, "exng": exng, 
-                "slp": slp, "caa": caa, "thall": thall+1
+                "age": age,
+                "sex": sex,
+                "cp": cp,
+                "trtbps": trtbps,
+                "chol": chol,
+                "fbs": fbs,
+                "restecg":restecg,
+                "thalachh": thalach,
+                "exng": exng,
+                "oldpeak": oldpeak,
+                "slp": slp,
+                "caa": caa,
+                "thall": thall+1
             }
             input_df = pd.DataFrame([input_data])
-            print("Input DataFrame Columns:", input_df.columns)
-            print("input DataFrame:", input_df, sep="\n")
+
+            scaler = joblib.load("scaler_cp_trtbps_exng_slp_caa_thall.pkl")
+
+            scaled_array = scaler.transform(input_df)
+            input_df_scaled = pd.DataFrame(scaled_array, columns=input_df.columns)
+
+            print("Input DataFrame Columns:", input_df_scaled.columns)
+            print("input DataFrame:", input_df_scaled, sep="\n")
+
+
+            required_cols = ['cp', 'trtbps', 'exng', 'slp', 'caa', 'thall']
             try:
-                input_df = input_df[['cp', 'trtbps', 'exng', 'slp', 'caa', 'thall']]
+                input_df_scaled = input_df_scaled[required_cols]
             except KeyError as e:
                 st.error(f"Missing column: {e}")
                 st.stop()
 
-            scaler = joblib.load("scaler_cp_trtbps_exng_slp_caa_thall.pkl")
-
-            input_df_scaled = scaler.transform(input_df)
+            
 
             prediction = model.predict(input_df_scaled)[0]
 
